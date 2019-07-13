f4170e6137921fb8c8c01ebfadadef31f20897bb
kalemena <kalemena@free.fr>
2019-07-13 17:47:32 +0200

    update sample code with button to activate motors

--- stories/11-RC-mower-test/RC-mower-master/RC-mower-master.ino
+++ stories/11-RC-mower-test/RC-mower-master/RC-mower-master.ino
@@ -34,36 +34,42 @@ void loop() {
   // Channels:
   // - 2 for speed
   // - 0 for direction
+  // - 7 activate mower
   // - 8 for mower rotor speed
 
-  int ctrlSpeed = iBus.get_channel(2); // 1000 to 2000
-  int ctrlDir = iBus.get_channel(0);   // 1000 to 2000, centered at 1500
-  int ctrlRotorSpeed = iBus.get_channel(8);   // 1000 to 2000
-  
-  int computedSpeed = (ctrlSpeed - 1500) / 2; // -250 to +250
-  int computedDir = (ctrlDir - 1500) / 2; // -250 to +250
-  
-  int computedSpeedLeft, computedSpeedRight;
-
-  if(computedSpeed > 0) {
-    // forward
-    computedSpeedLeft = computedSpeed + computedDir;
-    computedSpeedRight = computedSpeed - computedDir;
-  } else {
-    // backward
-    computedSpeedLeft = computedSpeed - computedDir;
-    computedSpeedRight = computedSpeed + computedDir;
-  }
+  int ctrlActivate = iBus.get_channel(7); // 1000 or 2000
+  if(ctrlActivate < 1500) {
+    int ctrlSpeed = iBus.get_channel(2); // 1000 to 2000
+    int ctrlDir = iBus.get_channel(0);   // 1000 to 2000, centered at 1500
+    int ctrlRotorSpeed = iBus.get_channel(8);   // 1000 to 2000
     
-  Serial.print("Speed = ");
-  Serial.print(computedSpeed);
-  Serial.print("\tDir = ");
-  Serial.print(computedDir);
-  Serial.print("\t(L=");
-  Serial.print(computedSpeedLeft);
-  Serial.print(",R=");
-  Serial.print(computedSpeedRight);
-  Serial.print(")\n");
-
-  sendRotorsAction(map(ctrlRotorSpeed, 1000, 2000, 0, 255), computedSpeedLeft, computedSpeedRight);
+    int computedSpeed = (ctrlSpeed - 1500) / 2; // -250 to +250
+    int computedDir = (ctrlDir - 1500) / 2; // -250 to +250
+    
+    int computedSpeedLeft, computedSpeedRight;
+  
+    if(computedSpeed > 0) {
+      // forward
+      computedSpeedLeft = computedSpeed + computedDir;
+      computedSpeedRight = computedSpeed - computedDir;
+    } else {
+      // backward
+      computedSpeedLeft = computedSpeed - computedDir;
+      computedSpeedRight = computedSpeed + computedDir;
+    }
+      
+    Serial.print("Speed = ");
+    Serial.print(computedSpeed);
+    Serial.print("\tDir = ");
+    Serial.print(computedDir);
+    Serial.print("\t(L=");
+    Serial.print(computedSpeedLeft);
+    Serial.print(",R=");
+    Serial.print(computedSpeedRight);
+    Serial.print(")\n");
+  
+    sendRotorsAction(map(ctrlRotorSpeed, 1000, 2000, 0, 255), computedSpeedLeft, computedSpeedRight);
+  } else {
+    sendRotorsAction(1, 0, 0);
+  } 
 }
